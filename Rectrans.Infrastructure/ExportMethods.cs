358bfc7bdb42c638f0c3d83c5089edfbbe000537
sou1maker <sou1maker@163.com.com>
2022-01-19 00:41:45 +0800

    :bug: 修复修改语言后未进行翻译的问题

--- Rectrans.Infrastructure/ExportMethods.cs
+++ Rectrans.Infrastructure/ExportMethods.cs
@@ -3,7 +3,8 @@
 public static class ImageTranslate
 {
     private static string? _original;
-    private static string? _translationg;
+    private static string? _translation;
+    private static string? _tl;
 
     public static async Task<(string, string, int)> ExecuteAsync(double x, double y, double height,
         double width, string sl, string tl)
@@ -12,14 +13,17 @@ public static class ImageTranslate
         var sourceText = Identify.FromScreen(x, y, height, width, sl);
 
         if (_original == null
-            || _translationg == null
-            || _original != sourceText)
+            || _translation == null
+            || _tl == null
+            || _original != sourceText
+            || _tl != tl)
         {
             _original = sourceText;
-            _translationg = await Google.ProcessAsync(sourceText, sl, tl);
+            _translation = await Google.ProcessAsync(sourceText, sl, tl);
+            _tl = tl;
             count = _original.Length;
         }
 
-        return (_original, _translationg, count);
+        return (_original, _translation, count);
     }
 }
